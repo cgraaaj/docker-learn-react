23f2124fe367ed99c4179bf07459a4d12bfd3357
pudge <cgrvel@gmail.com>
2021-06-13 15:21:33 +0530

    made some changes

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >learn react</a>
+        >changed on feature change</a>
       </header>
     </div>
   );
